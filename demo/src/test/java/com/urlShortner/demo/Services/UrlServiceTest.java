8af3a5fa5ceaf07662d78bebcfb123c0099c416b
dhanrjmetkar <dhanrajmetkar3660@gmail.com>
2024-05-02 17:43:26 +0530

    working on test cases

---
+++ demo/src/test/java/com/urlShortner/demo/Services/UrlServiceTest.java
@@ -0,0 +1,24 @@
+package com.urlShortner.demo.Services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UrlServiceTest {
+
+    @Test
+    void getAllUrl() {
+    }
+
+    @Test
+    void getUrl() {
+    }
+
+    @Test
+    void addurl() {
+    }
+
+    @Test
+    void getcount() {
+    }
+}
